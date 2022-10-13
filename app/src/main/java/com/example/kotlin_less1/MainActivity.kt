d3ec558dd15d5e876b2902db0e58e5f102292503
gibord <gibord003@gmail.com>
2022-10-13 16:53:41 +0300

    Повесил ClickListener на кнопку

--- app/src/main/java/com/example/kotlin_less1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_less1/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.kotlin_less1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
 import com.example.kotlin_less1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<AppCompatButton>(R.id.button1).setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+            }
+        } )
+
+
     }
 }
